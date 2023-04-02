0abe995e1c6ef09e76f3d60499f0978f02aadb9d
venkat <venkat.mobiqa@gmail.com>
2023-04-02 11:42:29 +0530

    updating ./tf-module-rds1

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
+variable "num_cache_nodes" {}
+variable "node_type" {}
 variable "engine_version" {}
-variable "number_of_instances" {}
-variable "instance_class" {}
-variable "engine" {}
+
